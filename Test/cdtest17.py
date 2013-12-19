764ec15ebd13766eb742258c6dceb69b9a116d62
Charles Doutriaux <doutriaux1@llnl.gov>
2013-12-19 12:03:03 -0800

    Migrated more cdms2 tests

--- Test/cdtest17.py
+++
@@ -1,8 +0,0 @@
-import sys,cdms2
-print 'Test 17: Slicing and keeping dims ...',
-f=cdms2.open(sys.prefix+'/sample_data/clt.nc')
-s=f("clt")
-s0=s[0]
-assert s0.getAxis(0).isLatitude() == True
-assert s0.getAxis(1).isLongitude() == True
-print 'OK'

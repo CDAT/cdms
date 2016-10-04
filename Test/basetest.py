e8595b9f7c849906d9d50f2208fe464966cd402b
Sam Fries <fries2@llnl.gov>
2016-10-04 09:20:18 -0700

    Added convenience methods to basetest, converted cdtest03

--- Test/basetest.py
+++ Test/basetest.py
@@ -2,10 +2,23 @@ import unittest
 import shutil
 import numpy
 import tempfile
+import cdms2
+import os
 
 
 class CDMSBaseTest(unittest.TestCase):
+    def getFile(self, path, mode="r"):
+        f = cdms2.open(path, mode)
+        self.files.append(f)
+        return f
+
+    def getTempFile(self, path, mode="r"):
+        f = cdms2.open(os.path.join(self.tempdir, path), mode)
+        self.files.append(f)
+        return f
+
     def setUp(self):
+        self.files = []
         self.NTIME = 3
         self.NLAT = 16
         self.NLON = 32
@@ -14,4 +27,6 @@ class CDMSBaseTest(unittest.TestCase):
         self.tempdir = tempfile.mkdtemp()
 
     def tearDown(self):
+        for f in self.files:
+            f.close()
         shutil.rmtree(self.tempdir)

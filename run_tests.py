b39a0988dcc8a458d55a980654e487b5bf03e909
Charles Doutriaux <doutriaux1@llnl.gov>
2016-10-26 14:34:51 -0700

    test can be passed as full path to it

--- run_tests.py
+++ run_tests.py
@@ -6,12 +6,17 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Test"))
 if len(sys.argv)==1:
-    tests = glob.glob("Test/test_*.py")
-    names = [t[5:-3] for t in tests]
-    names.remove("test_cdscan")
-    names.insert(0,"test_cdscan")
+    names = glob.glob("Test/test_*.py")
 else:
     names = sys.argv[1:]
+
+for i,nm in enumerate(names):
+    nm = nm.split("/")[-1].split(".py")[0]
+    names[i]=nm
+
+if "test_cdscan" in names:
+    names.remove("test_cdscan")
+    names.insert(0,"test_cdscan")
 
 suite = unittest.defaultTestLoader.loadTestsFromNames(names)
 runner = unittest.TextTestRunner(verbosity=2).run(suite)

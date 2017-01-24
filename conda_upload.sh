c014999ae1dd2f74bad9287b707f8f746cd83064
Charles Doutriaux <doutriaux1@llnl.gov>
2017-01-24 14:28:17 -0800

    while testing turning off other numpys

--- conda_upload.sh
+++ conda_upload.sh
@@ -13,10 +13,10 @@ export VERSION=`date +%Y.%m.%d`
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 python ./prep_for_build.py -v `date +%Y.%m.%d`
-conda build cdms2 -c conda-forge -c uvcdat --numpy=1.11
+conda build -c conda-forge -c uvcdat --numpy=1.11 cdms2
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np111py27_0.tar.bz2 --force
-conda build cdms2 -c conda-forge -c uvcdat --numpy=1.10
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np110py27_0.tar.bz2 --force
-conda build cdms2 -c conda-forge -c uvcdat --numpy=1.9
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np19py27_0.tar.bz2 --force
+#conda build cdms2 -c conda-forge -c uvcdat --numpy=1.10
+#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np110py27_0.tar.bz2 --force
+#conda build cdms2 -c conda-forge -c uvcdat --numpy=1.9
+#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np19py27_0.tar.bz2 --force
 

152c6175483104fe67e1056778e66fe4eafcfe50
Charles Doutriaux <doutriaux1@llnl.gov>
2017-01-25 16:50:20 -0800

    installing missing packages on centos6

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 PKG_NAME=cdms2
 USER=uvcdat
+echo "Trying to upload conda"
 if [ `uname` == "Linux" ]; then
     OS=linux-64
-    wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+    echo "Linux OS"
+    yum install -y wget git redhat-lsb-core
+    wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh --no-check-certificate
+    echo "WGET RETRUNED:"$?
     export PATH="$HOME/miniconda/bin:$PATH"
     bash miniconda.sh -b -p $HOME/miniconda
     conda config --set always_yes yes --set changeps1 no
+    conda config --set ssl_verify false
     conda update -y -q conda
     conda install gcc conda-build anaconda-client
+    binstar config --set verify_ssl False
 else
+    echo "Mac OS"
     OS=osx-64
 fi
 
@@ -17,10 +24,13 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
+echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 python ./prep_for_build.py -v `date +%Y.%m.%d`
+echo "Building now"
 conda build -c conda-forge -c uvcdat --numpy=1.11 cdms2
+echo "Uploading"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np111py27_0.tar.bz2 --force
 #conda build cdms2 -c conda-forge -c uvcdat --numpy=1.10
 #anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np110py27_0.tar.bz2 --force

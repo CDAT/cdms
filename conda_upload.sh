deef08f378ebbf5027fea805ba2d0a583efb4377
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-09 14:56:49 -0800

    get latest docker image with conda-forge pkg pre-downloaded
    build numpy 1.11, 1.10 , 1.9

--- conda_upload.sh
+++ conda_upload.sh
@@ -19,14 +19,15 @@ export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
+# uvcdat creates issues for build -c uvcdat confises package and channel
+rm -rf uvcdat
 python ./prep_for_build.py -v `date +%Y.%m.%d`
 echo "Building now"
-echo "ENV"`env`
 conda build -c conda-forge -c uvcdat --numpy=1.11 cdms2
 echo "Uploading"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np111py27_0.tar.bz2 --force
-#conda build cdms2 -c conda-forge -c uvcdat --numpy=1.10
-#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np110py27_0.tar.bz2 --force
-#conda build cdms2 -c conda-forge -c uvcdat --numpy=1.9
-#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np19py27_0.tar.bz2 --force
+conda build cdms2 -c conda-forge -c uvcdat --numpy=1.10
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np110py27_0.tar.bz2 --force
+conda build cdms2 -c conda-forge -c uvcdat --numpy=1.9
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-np19py27_0.tar.bz2 --force
 

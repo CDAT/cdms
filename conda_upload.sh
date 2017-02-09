a62014741e46d425b370f30ea6f10a42553acb34
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-09 11:44:18 -0800

    trying to use our custom docker image with everything pre-loaded

--- conda_upload.sh
+++ conda_upload.sh
@@ -5,16 +5,8 @@ echo "Trying to upload conda"
 if [ `uname` == "Linux" ]; then
     OS=linux-64
     echo "Linux OS"
-    yum install -y wget git redhat-lsb-core gcc-gfortran
-    wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh --no-check-certificate
-    echo "WGET RETRUNED:"$?
-    export PATH="$HOME/miniconda/bin:$PATH"
-    bash miniconda.sh -b -p $HOME/miniconda
-    conda config --set always_yes yes --set changeps1 no
-    conda config --set ssl_verify false
+    export PATH="$HOME/miniconda2/bin:$PATH"
     conda update -y -q conda
-    conda install gcc conda-build anaconda-client libgcc libgfortran
-    binstar config --set verify_ssl False
 else
     echo "Mac OS"
     OS=osx-64
@@ -22,7 +14,7 @@ fi
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
-export CONDA_BLD_PATH=~/conda-bld
+export CONDA_BLD_PATH=${HOME}/conda-bld
 export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes

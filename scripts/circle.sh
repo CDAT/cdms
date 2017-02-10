6c93846e18213a07ff4c9ffa24bdc450767611e6
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-09 16:42:20 -0800

    reorg a bit

--- scripts/circle.sh
+++ scripts/circle.sh
@@ -8,7 +8,8 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
 export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
-ls -l 
+cd /git_repo
+ls -l
 #git clone git://github.com/UV-CDAT/conda-recipes
 #cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel

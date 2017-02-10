9275b121f3944e8fa779d658ab2f72df433c19f8
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-09 16:49:20 -0800

    ok actually running this thing now on circle-ci

--- scripts/circle.sh
+++ scripts/circle.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 PKG_NAME=cdms2
 USER=uvcdat
+export PATH=${HOME}/miniconda2/bin:${PATH}
 echo "Trying to upload conda"
 conda update -y -q conda
-mkdir ${HOME}/conda-bld
+conda install -c conda-forge -c uvcdat uvcdat-nox pyopenssl
+vcs_download_sample_data
+#mkdir ${HOME}/conda-bld
 conda config --set anaconda_upload no
-export CONDA_BLD_PATH=${HOME}/conda-bld
-export VERSION=`date +%Y.%m.%d`
+#export CONDA_BLD_PATH=${HOME}/conda-bld
+#export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
 cd /git_repo
 ls -l
+python setup.py install
+python run_tests.py
 #git clone git://github.com/UV-CDAT/conda-recipes
 #cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel

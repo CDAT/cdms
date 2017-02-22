75d4f3668aa9175fde12dc1479597a299f39200e
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-22 09:08:16 -0800

    trying to get circleci to upload mac

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -1,4 +1,7 @@
 export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
+echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
+echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
+echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 python run_tests.py 
-
+if [ $_ == 0 ]; then ./conda_upload.sh

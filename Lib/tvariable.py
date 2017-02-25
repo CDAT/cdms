bcb0329e7c654b8706a8f4f60973e8d864e02951
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-02-24 18:23:39 -0800

    Fix #4 _FillValue exist and missing_value does not in file

--- Lib/tvariable.py
+++ Lib/tvariable.py
@@ -202,7 +202,7 @@ class TransientVariable(AbstractVariable, numpy.ma.MaskedArray):
             for key, value in attributes.items():
                 if (key in ['shape', 'flat', 'imaginary', 'real'] or key[0] == '_') and key not in ['_FillValue']:
                     raise CDMSError('Bad key in attributes: ' + key)
-                elif key == 'missing_value':
+                elif (key == 'missing_value' or key == '_FillValue'):
                     # ignore if fill value given explicitly
                     if fill_value is None:
                         self._setmissing(value)

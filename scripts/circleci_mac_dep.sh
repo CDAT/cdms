705894aa59a33602bccabd7c0b04b505271dcfa0
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-02-28 18:26:10 -0800

    make sure with get uvcdat/label/nightly first and that return 0 for circle ci

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -q -c conda-forge -c uvcdat/label/nightly -c uvcdat uvcdat pyopenssl nose #image-compare
+conda install -q  -c uvcdat/label/nightly -c conda-forge -c uvcdat vcs pyopenssl nose #image-compare
 export UVCDAT_ANONYMOUS_LOG=False
 vcs_download_sample_data
 python setup.py install

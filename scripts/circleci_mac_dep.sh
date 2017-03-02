ff4764065795dcaed96bba62d51d2ce919f25cd4
Sam Fries <fries2@llnl.gov>
2017-03-02 13:42:58 -0800

    Added compute-graph to deps

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c conda-forge -c uvcdat uvcdat pyopenssl nose #image-compare
+conda install -c conda-forge -c uvcdat uvcdat pyopenssl nose compute-graph #image-compare
 export UVCDAT_ANONYMOUS_LOG=False
 vcs_download_sample_data
 python setup.py install

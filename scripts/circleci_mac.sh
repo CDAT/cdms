cc83c081b7a09cf1c2c7600a228d93dd6251ac6a
Charles Doutriaux <doutriaux1@llnl.gov>
2017-06-26 11:12:05 -0700

    fixing nightlies names and circleci passing even when fail (#145)
    * fixing nightlies names and circleci passing even when fail
    
    * making it write to see why it fails
    
    * adding version
    
    * ok only for master now

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -4,6 +4,6 @@ echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
 echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 python run_tests.py -v2 -s
-rc=$?
-if [ ${rc} -ne 0 ]; then exit ${rc} ; fi
-if [ ${rc} -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
+RESULT=$?
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
+exit $RESULT

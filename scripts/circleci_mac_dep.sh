d6b346ed70b22f3a8a6f7561a9e5814a90ce580d
Denis <nadeau1@llnl.gov>
2017-09-26 20:33:58 -0700

    change circleci dods certificate

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -28,6 +28,18 @@ cmd="echo ${ESGF_PWD} | myproxyclient logon -s esgf-node.llnl.gov -p 7512 -t 12 
 $cmd
 stty echo
 
+cmd="openssl pkcs12 -export -inkey /Users/distiller/.esg/esgf.cert -in /Users/distiller/.esg/esgf.cert -name esgf  -out /Users/distiller/.esg/esgf.p12 -passout pass:esgf"
+$cmd
+
+cmd="sudo security import /Users/distiller/.esg/esgf.p12 -A -P esgf -k "/Library/Keychains/System.keychain" 
+echo $cmd
+$cmd
+
+cmd="sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" /Users/distiller/.esg/esgf.cert" 
+echo $cmd
+$cmd
+
+
 cmd="cp tests/dodsrccircleci /Users/distiller/.dodsrc"
 echo $cmd
 $cmd

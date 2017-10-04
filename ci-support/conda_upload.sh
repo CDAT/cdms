e4cc40635e87afa7bbe4aa3e43abcee469bc9de0
Denis <nadeau1@llnl.gov>
2017-10-03 23:26:08 -0700

    uncomment env portion

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -15,12 +15,13 @@ fi
 # Python 3 section
 echo "Building python 3"
 source activate py3
-mkdir ~/conda-bld
+mkdir ${HOME}/conda-bld
 conda install -n root -q anaconda-client conda-build
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
 export VERSION="2.12"
 echo "Cloning recipes"
+cd ${HOME}
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
@@ -38,10 +39,14 @@ anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$
 # Python 2 section
 echo "Building python 2"
 source activate py2
+which python
 conda install -q anaconda-client conda-build
 conda config --set anaconda_upload no
+rm -rf ${HOME}/conda-bld
+mkdir ${HOME}/conda-bld
 export CONDA_BLD_PATH=${HOME}/conda-bld
 export VERSION="2.12"
+cd ${HOME}/conda-recipes
 python ./prep_for_build.py
 echo "Building now"
 conda build $PKG_NAME -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.13

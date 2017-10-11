f7196f366308060292841f13630bb79d6b62fce3
Charles Doutriaux <doutriaux1@llnl.gov>
2017-10-11 11:14:44 -0700

    trying to see if upload broken (#175)
    * trying to see if upload broken
    
    * correct path
    
    * add python2 and python3 to circle ci
    
    * create py2 env fixing
    
    * bash arithmetic needs parenthesis
    
    * install conda-build in root env
    
    * try another miniconda and change conda_upload
    
    * use nesii channel for py3
    
    * add nadeau1 channel for py3 on OSX for CircleCI
    
    * change py27 to py36 in upload filename
    
    * get rid of docker for now
    
    * add then to condition
    
    * force upload
    
    * change directory
    
    * change directory
    
    * change directory
    
    * change directory
    
    * change directory
    
    * uncomment env portion
    
    * py3 insure python=3
    
    * do not update conda
    
    * pin conda for conda-bld
    
    * pin conda for conda-bld
    
    * try to pin python
    
    * fix linxu typo
    
    * add space before _
    
    * remove PYTHON_SYSCONFIG env var
    
    * reactivate docker
    
    * print STDERR from docker
    
    * change HOME to travis_homei PATH for docker
    
    * change pwd to /home/travis
    
    * change upload path
    
    * use centos:6.8 docker for travis
    
    * use centos:6.8 docker for travis
    
    * add always_yes to docker for conda
    
    * fix if-then-else condition
    
    * add git to centos 6.u8
    
    * need to add gcc in OS with yum
    
    * remove wget hash ##

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -3,7 +3,11 @@ export PATH=${HOME}/miniconda/bin:${PATH}
 echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
 echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
+source activate py2
 python run_tests.py -v2 -s
 RESULT=$?
-if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
+source activate py3
+python run_tests.py -v2 -s
+RESULT=$(( $RESULT + $? ))
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH != "master" ]; then bash ./ci-support/conda_upload.sh ; fi
 exit $RESULT
---
+++ ci-support/circleci_mac_dep.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+cmd="ls"
+echo $cmd
+$cmd
+
+cmd="pwd"
+echo $cmd
+$cmd
+
+cmd="export PATH=${HOME}/miniconda/bin:${PATH}"
+echo $cmd
+$cmd
+
+# Create Python 3 environment
+cmd="conda create -n py3 -c uvcdat/label/nightly -c conda-forge -c uvcdat libcf distarray cdtime libcdms cdat_info numpy esmf esmpy libdrs_f pyopenssl nose requests flake8 myproxyclient"
+echo $cmd
+$cmd
+
+cmd="conda install -n py3 -c nadeau1 esmf esmpy"
+echo $cmd
+$cmd
+
+# Create Python 2 environment
+cmd="conda create -n py2 python=2.7"
+echo $cmd
+$cmd
+
+# Activate python 2 environment
+cmd="source activate py2"
+echo $cmd
+$cmd 
+
+cmd="conda install -n py2  -c uvcdat/label/nightly -c conda-forge -c uvcdat libcf distarray cdtime libcdms cdat_info numpy esmf esmpy libdrs_f pyopenssl nose requests flake8 myproxyclient"
+echo $cmd
+$cmd
+
+# add relative path to ncdump
+cmd="install_name_tool -change /usr/lib/libcurl.4.dylib @rpath/libcurl.4.dylib /Users/distiller/miniconda/env/py2/bin/ncdump"
+echo $cmd
+$cmd 
+
+cmd="install_name_tool -change /usr/lib/libcurl.4.dylib @rpath/libcurl.4.dylib /Users/distiller/miniconda/env/py3/bin/ncdump"
+echo $cmd
+$cmd 
+
+cmd="export UVCDAT_ANONYMOUS_LOG=False"
+echo $cmd
+$cmd
+
+
+# Retrieve certificates from ESGF
+cmd="mkdir /Users/distiller/.esg"
+echo $cmd
+$cmd
+
+cmd="echo ${ESGF_PWD} | myproxyclient logon -s esgf-node.llnl.gov -p 7512 -t 12 -S -b -l ${ESGF_USER} -o /Users/distiller/.esg/esgf.cert "
+eval $cmd
+
+cmd="openssl pkcs12 -export -inkey /Users/distiller/.esg/esgf.cert -in /Users/distiller/.esg/esgf.cert -name esgf  -out /Users/distiller/.esg/esgf.p12 -passout pass:esgf"
+$cmd
+
+cmd="sudo security import /Users/distiller/.esg/esgf.p12 -A -P esgf -k /Library/Keychains/System.keychain"
+echo $cmd
+$cmd
+
+cmd="sudo security add-trusted-cert -d -r trustRoot -k '/Library/Keychains/System.keychain' /Users/distiller/.esg/esgf.cert" 
+echo $cmd
+$cmd
+
+cmd="cp tests/dodsrccircleci /Users/distiller/.dodsrc"
+echo $cmd
+$cmd
+
+# compile cdms on py2 and py3 environemt.
+
+cmd="python setup.py install"
+echo $cmd
+$cmd
+
+cmd="source activate py3"
+echo $cmd
+$cmd 
+
+cmd="python setup.py install"
+echo $cmd
+$cmd

7bfeee21b2de9ea6203407729f44e06b8061e499
Charles Doutriaux <doutriaux1@llnl.gov>
2017-10-25 06:51:00 -0700

    trying to see if building with miniconda2 will fix compatibility issue

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -6,8 +6,8 @@ if [ `uname` == "Linux" ]; then
     OS=linux-64
     echo "Linux OS"
     yum install -y wget git gcc
-    wget --no-check https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  -O miniconda3.sh 2> /dev/null
-    bash miniconda3.sh -b -p $HOME/miniconda
+    wget --no-check https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh  -O miniconda2.sh 2> /dev/null
+    bash miniconda2.sh -b -p $HOME/miniconda
     export PATH=$HOME/miniconda/bin:$PATH
     echo $PATH
     which python

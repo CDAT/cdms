a6ce9391e0ef7d4f1b98767ca3fea3d1a5d5a430
Charles Doutriaux <doutriaux1@llnl.gov>
2017-10-25 07:00:53 -0700

    why is it not trigerring on circleci?

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -9,5 +9,6 @@ RESULT=$?
 source activate py3
 python run_tests.py -v2 -s
 RESULT=$(( $RESULT + $? ))
+echo "RESULT:"${RESULT}
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH != "master" ]; then bash ./ci-support/conda_upload.sh ; fi
 exit $RESULT

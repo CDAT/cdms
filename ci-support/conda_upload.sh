a8eeeff7a7696fdd067404d0e2f8231166ab1c3b
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-11-06 10:28:55 -0800

    Upload py2py3 (#188)
    * fix upload for python3 and numpy using miniconda3
    
    * pinned to miniconda3 directory
    
    * install anaconda-client in both miniconda(s)
    
    * install conda-build in miniconda2
    
    * build branch
    
    * purge conda-build and clean all
    
    * remove BUILD_PATH
    
    * remove CONDA_BLD_PATH
    
    * change bld path to /home/nadeau1/miniconda
    
    * remove np??? from upload filename
    
    * fix conda_upload.sh
    
    * change back circleci branch to master
    
    * put back CONDA_BLD_PATH for circleci
    
    * change Darwin miniconda path
    
    * fix conda_upload with new variants
    
    * fix conda_upload if command
    
    * fix conda_upload ncdump
    
    * fixed and revert to master

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -10,11 +10,9 @@ if [ `uname` == "Linux" ]; then
     echo "Linux OS"
     yum install -y wget git gcc
     wget --no-check https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  -O miniconda3.sh 2> /dev/null
-    wget --no-check https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh  -O miniconda2.sh 2> /dev/null
-    bash miniconda3.sh -b -p ${HOME}/miniconda3
-    bash miniconda2.sh -b -p ${HOME}/miniconda2
+    bash miniconda3.sh -b -p ${HOME}/miniconda
     export SYSPATH=$PATH
-    export PATH=${HOME}/miniconda3/bin:${SYSPATH}
+    export PATH=${HOME}/miniconda/bin:${SYSPATH}
     echo $PATH
     conda install -n root -q anaconda-client conda-build
     conda install -n root gcc future
@@ -35,10 +33,6 @@ if [ `uname` == "Linux" ]; then
     cp tests/dodsrc ${HOME}.dodsrc
     source deactivate
 # Python 2.7 environment
-    export PATH=${HOME}/miniconda2/bin:${SYSPATH}
-    echo $PATH
-    conda install -n root -q anaconda-client conda-build
-    conda install -n root gcc future
     echo "Creating python 2 env"
     conda create -n py2 python=2.7
     conda install -n py2 -c conda-forge -c uvcdat libcf distarray cdtime libcdms cdat_info numpy esmf esmpy libdrs_f pyopenssl nose requests flake8 numpy
@@ -49,16 +43,14 @@ else
 fi
 
 # Python 3 section
-if [ `uname` == "Linux" ]; then
-    export PATH=${HOME}/miniconda3/bin:${SYSPATH}
-    echo $PATH
-fi
 echo "Building python 3"
 source activate py3
 which python
 conda install -n root -q anaconda-client conda-build
 # pin conda so that conda-build does not update it
-echo "conda ==4.3.21" >> ~/miniconda3/conda-meta/pinned  # Pin conda as workaround for conda/conda#6030
+if [ `uname` == "Darwin" ]; then
+    echo "conda ==4.3.21" >> ~/miniconda/conda-meta/pinned  # Pin conda as workaround for conda/conda#6030
+fi
 conda config --set anaconda_upload no
 echo "Cloning recipes"
 cd ${HOME}
@@ -66,21 +58,15 @@ git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
 rm -rf uvcdat
-python ./prep_for_build.py -b ${TRAVIS_BRANCH}
+python ./prep_for_build.py
 echo "Building now"
 echo "use nesii/label/dev-esmf for py3"
 CONDA_PY=36 conda build $PKG_NAME -c nesii/label/dev-esmf -c nadeau1 -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.13
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*-py3*_0.tar.bz2 --force
 CONDA_PY=36 conda build $PKG_NAME -c nesii/label/dev-esmf -c nadeau1 -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.12
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*-py3*_0.tar.bz2 --force
 CONDA_PY=36 conda build $PKG_NAME -c nesii/label/dev-esmf -c nadeau1 -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.11
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*-py3*_0.tar.bz2 --force
 
 # Python 2 section
-if [ `uname` == "Linux" ]; then
-    export PATH=${HOME}/miniconda2/bin:${SYSPATH}
-    echo $PATH
-fi
 echo "Building python 2"
 source activate py2
 which python
@@ -89,9 +75,7 @@ conda config --set anaconda_upload no
 cd ${HOME}/conda-recipes
 echo "Building now"
 CONDA_PY=27 conda build $PKG_NAME -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.13
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*-py27*_0.tar.bz2 --force
 CONDA_PY=27 conda build $PKG_NAME -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.12 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*-py27*_0.tar.bz2 --force
 CONDA_PY=27 conda build $PKG_NAME -c uvcdat/label/nightly -c conda-forge -c uvcdat --numpy=1.11 
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*-py27*_0.tar.bz2 --force
 

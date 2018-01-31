c1479f026b56ec98875ee72618b5b8bffceb570b
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-01-30 16:42:15 -0800

    fix upload and add label

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -10,6 +10,6 @@ source activate py3
 python run_tests.py -v2 -s
 RESULT=$(( $RESULT + $? ))
 echo "RESULT:"${RESULT}
-if [ $RESULT -eq 0 ]; then bash ./ci-support/conda_upload.sh ; fi
-#if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./ci-support/conda_upload.sh ; fi
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then LABEL='unstable' bash ./ci-support/conda_upload.sh ; fi
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then LABEL='nightly' bash ./ci-support/conda_upload.sh ; fi
 exit $RESULT
--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -9,39 +9,42 @@ if [ `uname` == "Linux" ]; then
     OS=linux-64
     echo "Linux OS"
     yum install -y wget git gcc
-    wget --no-check https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  -O miniconda3.sh 2> /dev/null
-    bash miniconda3.sh -b -p ${HOME}/miniconda
+    # wget --no-check https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  -O miniconda3.sh 2> /dev/null
+    wget --no-check https://repo.continuum.io/miniconda/Miniconda2-4.3.30-Linux-x86_64.sh  -O miniconda2.sh 2> /dev/null
+    bash miniconda2.sh -b -p ${HOME}/miniconda
     export SYSPATH=$PATH
     export PATH=${HOME}/miniconda/bin:${SYSPATH}
     echo $PATH
-    conda install -n root -q anaconda-client conda-build
-    conda install -n root gcc future
     conda config --set always_yes yes --set changeps1 no
+    conda config --set anaconda_upload false --set ssl_verify false
+    conda install -n root -q anaconda-client "conda-build<3.3"
+    conda install -n root gcc future
     which python
     export UVCDAT_ANONYMOUS_LOG=False
-    echo "Creating python 3 env"
-    conda create -n py3 python=3.6
-    conda install -n py3 -c conda-forge -c uvcdat setuptools libcf distarray cdtime libcdms cdat_info numpy libdrs_f pyopenssl nose requests flake8 myproxyclient numpy
-    conda install -n py3 -c nesii/channel/dev-esmf -c conda-forge esmpy
-    echo "Creating certificate"
-    source activate py3
-    mkdir ${HOME}/.esg
-    echo ${ESGF_PWD} | myproxyclient logon -s esgf-node.llnl.gov -p 7512 -t 12 -S -b -l ${ESGF_USER} -o ${HOME}/.esg/esgf.cert
-    ls ${HOME}/.esg
-    cd travis_home
-    ls
-    cp tests/dodsrc ${HOME}.dodsrc
-    source deactivate
+#    echo "Creating python 3 env"
+#    conda create -n py3 python=3.6
+#    conda install -n py3 -c conda-forge -c uvcdat setuptools libcf distarray cdtime libcdms cdat_info numpy libdrs_f pyopenssl nose requests flake8 myproxyclient numpy
+#    conda install -n py3 -c nesii/channel/dev-esmf -c conda-forge esmpy
+#    echo "Creating certificate"
+#    source activate py3
+#    mkdir ${HOME}/.esg
+#    echo ${ESGF_PWD} | myproxyclient logon -s esgf-node.llnl.gov -p 7512 -t 12 -S -b -l ${ESGF_USER} -o ${HOME}/.esg/esgf.cert
+#    ls ${HOME}/.esg
+#    cd travis_home
+#    ls
+#    cp tests/dodsrc ${HOME}.dodsrc
+#    source deactivate
 else
     echo "Mac OS"
     OS=osx-64
 fi
 
-# Python 3 section
-echo "Building python 3"
-source activate py3
 which python
-conda install -n root -q anaconda-client conda-build
+if [ `uname` == "Linux" ]; then
+    conda install -n root -q anaconda-client "conda-build<3.3"
+else
+    conda install -n root -q anaconda-client conda-build
+fi
 # pin conda so that conda-build does not update it
 #if [ `uname` == "Darwin" ]; then
 #    echo "conda ==4.3.21" >> ~/miniconda/conda-meta/pinned  # Pin conda as workaround for conda/conda#6030
@@ -55,13 +58,9 @@ cd conda-recipes
 rm -rf uvcdat
 python ./prep_for_build.py
 echo "Building now"
-echo "use nesii/label/dev-esmf for py3"
-source activate py2
-which python
-conda install -n root -q anaconda-client conda-build
-conda config --set anaconda_upload no
-echo "Building now"
-echo "use nesii/label/dev-esmf for py3"
+echo "use nesii/label/dev-esmf for esmf"
+conda build -V
 conda build $PKG_NAME -c nesii/label/dev-esmf  -c uvcdat/label/nightly -c conda-forge -c uvcdat 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*_0.tar.bz2 --force
+binstar config --set verify_ssl false
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l ${LABEL}  ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*_0.tar.bz2 --force
 

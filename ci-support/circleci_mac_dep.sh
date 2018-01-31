ba211d45cc61f18b9aaa9a7411f527516b4b7171
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-01-31 13:24:00 -0800

    verify if conda-forge update their libnetcdf for OSX

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -46,13 +46,13 @@ $cmd
 :
 
 # add relative path to ncdump
-cmd="install_name_tool -change /usr/lib/libcurl.4.dylib @rpath/libcurl.4.dylib ${HOME}/miniconda/envs/py2/bin/ncdump"
-echo $cmd
-$cmd 
+#cmd="install_name_tool -change /usr/lib/libcurl.4.dylib @rpath/libcurl.4.dylib ${HOME}/miniconda/envs/py2/bin/ncdump"
+#echo $cmd
+#$cmd 
 
-cmd="install_name_tool -change /usr/lib/libcurl.4.dylib @rpath/libcurl.4.dylib ${HOME}/miniconda/envs/py3/bin/ncdump"
-echo $cmd
-$cmd 
+#cmd="install_name_tool -change /usr/lib/libcurl.4.dylib @rpath/libcurl.4.dylib ${HOME}/miniconda/envs/py3/bin/ncdump"
+#echo $cmd
+#$cmd 
 
 cmd="export UVCDAT_ANONYMOUS_LOG=False"
 echo $cmd
@@ -66,20 +66,20 @@ $cmd
 cmd="echo ${ESGF_PWD} | myproxyclient logon -s esgf-node.llnl.gov -p 7512 -t 12 -S -b -l ${ESGF_USER} -o /Users/distiller/.esg/esgf.cert "
 eval $cmd
 
-cmd="openssl pkcs12 -export -inkey /Users/distiller/.esg/esgf.cert -in /Users/distiller/.esg/esgf.cert -name esgf  -out /Users/distiller/.esg/esgf.p12 -passout pass:esgf"
-$cmd
+#cmd="openssl pkcs12 -export -inkey /Users/distiller/.esg/esgf.cert -in /Users/distiller/.esg/esgf.cert -name esgf  -out /Users/distiller/.esg/esgf.p12 -passout pass:esgf"
+#$cmd
 
-cmd="sudo security import /Users/distiller/.esg/esgf.p12 -A -P esgf -k /Library/Keychains/System.keychain"
-echo $cmd
-$cmd
+#cmd="sudo security import /Users/distiller/.esg/esgf.p12 -A -P esgf -k /Library/Keychains/System.keychain"
+#echo $cmd
+#$cmd
 
-cmd="sudo security add-trusted-cert -d -r trustRoot -k '/Library/Keychains/System.keychain' /Users/distiller/.esg/esgf.cert" 
-echo $cmd
-$cmd
+#cmd="sudo security add-trusted-cert -d -r trustRoot -k '/Library/Keychains/System.keychain' /Users/distiller/.esg/esgf.cert" 
+#echo $cmd
+#$cmd
 
-cmd="cp tests/dodsrccircleci /Users/distiller/.dodsrc"
-echo $cmd
-$cmd
+#cmd="cp tests/dodsrccircleci /Users/distiller/.dodsrc"
+#echo $cmd
+#$cmd
 
 # compile cdms on py2 and py3 environemt.
 

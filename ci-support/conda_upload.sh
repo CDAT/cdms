5a75e3fc974837913b973f2567c544824c00b9e9
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-05-30 18:18:10 -0700

    build cdms on circleci 2.0

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -1,71 +1,33 @@
 #!/usr/bin/env bash
-PKG_NAME=cdms2
+PKG_NAME=cdms
 USER=cdat
-echo "Trying to upload conda"
-mkdir ${HOME}/conda-bld
-export CONDA_BLD_PATH=${HOME}/conda-bld
 export VERSION="3.0"
+echo "Trying to upload to conda"
+echo ""
+echo "Activating base env"
+source activate base
+echo "Making sure conda-build is installed"
+conda install "conda-build<3.10"
+echo "Updating conda"
+conda update -y -q conda
 if [ `uname` == "Linux" ]; then
     OS=linux-64
     echo "Linux OS"
-    yum install -y wget git gcc
-    # wget --no-check https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  -O miniconda3.sh 2> /dev/null
-    wget --no-check https://repo.continuum.io/miniconda/Miniconda2-4.3.30-Linux-x86_64.sh  -O miniconda2.sh 2> /dev/null
-    bash miniconda2.sh -b -p ${HOME}/miniconda
-    export SYSPATH=$PATH
-    export PATH=${HOME}/miniconda/bin:${SYSPATH}
-    echo $PATH
-    conda config --set always_yes yes --set changeps1 no
-    conda config --set anaconda_upload false --set ssl_verify false
-    conda install -n root -q anaconda-client "conda-build<3.3"
-    conda install -n root gcc future
-    which python
-    export UVCDAT_ANONYMOUS_LOG=False
-    BRANCH=${TRAVIS_BRANCH}
 else
     echo "Mac OS"
     OS=osx-64
-    BRANCH=${CIRCLE_BRANCH}
 fi
 
-which python
-if [ `uname` == "Linux" ]; then
-    conda install -n root -q anaconda-client "conda-build<3.3"
-else
-    conda install -n root -q anaconda-client conda-build
-fi
+mkdir ~/conda-bld
 conda config --set anaconda_upload no
+export CONDA_BLD_PATH=${HOME}/conda-bld
 echo "Cloning recipes"
-cd ${HOME}
-git clone git://github.com/UV-CDAT/conda-recipes
+git clone git://github.com/CDAT/conda-recipes
 cd conda-recipes
-# cdat creates issues for build -c cdat confises package and channel
+# uvcdat creates issues for build -c uvcdat confises package and channel
 rm -rf uvcdat
-if [ `uname` == "Linux" ]; then
-    sed -i  's/last_stable = .*/last_stable="${VERSION}"/g' ./prep_for_build.py
-else
-    sed -i ''   's/last_stable = .*/last_stable="${VERSION}"/g' ./prep_for_build.py
-fi
-python ./prep_for_build.py -v ${VERSION} -b ${BRANCH}
-echo "Building now"
-echo "use nesii/label/dev-esmf for esmf"
-conda build -V
-conda build $PKG_NAME -c cdat/label/unstable   -c cdat/label/nightly -c conda-forge -c cdat 
-#
-# binstar config set 'false' instead of false (not quote) I have to do it manually
-# this is true for OSX.
-# binstar is changing verify_ssl to ssl_verify, but the later is not always working
-# 
-# binstar config --set verify_ssl false
-# binstar config --set ssl_verify false
-#
-mkdir -p ~/.continuum/anaconda-client/
-echo "ssl_verify: false" >> ~/.continuum/anaconda-client/config.yaml
-echo "verify_ssl: false" >> ~/.continuum/anaconda-client/config.yaml
-if [ `uname` == "Darwin" ]; then
-    # fix conda and anaconda-client conflict
-    conda install conda==4.2.16
-fi
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l ${LABEL}  ${CONDA_BLD_PATH}/$OS/$PKG_NAME-$VERSION.`date +%Y`*_0.tar.bz2 --force
+export BRANCH=${CIRCLE_BRANCH}
+python ./prep_for_build.py  -b ${BRANCH}
 
-
+conda build ${PKG_NAME} -c cdat/label/unstable -c conda-forge 
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $LABEL $CONDA_BLD_PATH/$OS/${PKG_NAME}-$VERSION.`date +%Y*`0.tar.bz2 --force

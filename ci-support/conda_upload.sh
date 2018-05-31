e06b8cb58bba69b5cd604b4a64c6e42082e1cf71
Denis Nadeau <denis.nadeau@gmail.com>
2018-05-30 23:58:07 -0700

    change Users/distiler to /Users/denisnadeau

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -29,5 +29,6 @@ rm -rf uvcdat
 export BRANCH=${CIRCLE_BRANCH}
 python ./prep_for_build.py  -b ${BRANCH}
 
-conda build ${PKG_NAME} -c cdat/label/unstable -c conda-forge 
+conda build ${PKG_NAME} -c cdat/label/unstable -c conda-forge  --python 3.6
+conda build ${PKG_NAME} -c cdat/label/unstable -c conda-forge  --python 2.7
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $LABEL $CONDA_BLD_PATH/$OS/${PKG_NAME}-$VERSION.`date +%Y*`0.tar.bz2 --force

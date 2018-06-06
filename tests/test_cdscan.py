a9e29eaaecd1158071660d4ce26ccafb56820639
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-06-05 18:14:24 -0700

    Netcdf46 (#249)
    * Fix python3 slice issue(setitem) and flake8
    
    * update to libnetcdf 4.6
    
    * try circleci unstable label
    
    * try version 2 circleci
    
    * try version 2 circleci
    
    * change workflow name
    
    * change cdtime to cdms
    
    * add certificate to circleci
    
    * add fix conda-upload in circleci 2.0
    
    * update prep_for_build version
    
    * fix curl command
    
    * fix cicleci for cdms
    
    * use unstable channel change uvcdat for cdat
    
    * build cdms on circleci 2.0
    
    * fix circleci config.yml
    
    * change Users/distiler to /Users/denisnadeau
    
    * add gcc_linux
    
    * add LDSHARED for linux
    
    * disable cert and py results
    
    * add gcc_linux-64
    
    * change cdscan link
    
    * fix myproxy
    
    * add esmf and esmpy to py3 env

--- tests/test_cdscan.py
+++ tests/test_cdscan.py
@@ -58,6 +58,7 @@ class TestCDScan(basetest.CDMSBaseTest):
             pass
         argv = 'cdscan -x test_dap.xml https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/Reanalysis/NASA-GMAO/GEOS-5/MERRA/mon/atmos/zg.ncml'.split()
         pth = cdat_info.get_sampledata_path()
+
         os.chdir(pth)
         cdscan(argv)
         f=cdms2.open("test_dap.xml")

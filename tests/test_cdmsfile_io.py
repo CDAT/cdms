ccc219dd0ca2f808c015b08b610d6ee634569cb9
Denis Nadeau <dnadeau4@users.noreply.github.com>
2019-04-11 16:40:49 -0700

    first attemp to fix multiple time dim write (#314)
    * first attemp to fix multiple time dim write
    
    * if indices are equal try again with INT_MAX
    
    * get rid of debug message

--- tests/test_cdmsfile_io.py
+++ tests/test_cdmsfile_io.py
@@ -11,11 +11,19 @@ class TestCDMSFileIO(basetest.CDMSBaseTest):
     def setUp(self):
         super(TestCDMSFileIO, self).setUp()
         self.readOnly = self.getDataFile("readonly.nc")
+        self.clt = self.getDataFile("clt.nc")['clt']
         self.u = self.readOnly["u"]
         self.u_masked = self.readOnly["umasked"]
 
     def tearDown(self):
         super(TestCDMSFileIO, self).tearDown()
+
+    def testZSS(self):
+        out_file=cdms2.open('temp.nc', 'w')
+        out_file.write(self.clt[0:10,:])
+        out_file.write(self.clt[10:20,:])
+        out_file.close()
+
 
     def testSize(self):
         self.assertEqual(self.u.size(), 512)

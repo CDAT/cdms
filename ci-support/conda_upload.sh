6ad4edeecfb7556b0ea5f687db5b04335bbe7fac
muryanto1 <muryanto1@llnl.gov>
2019-09-24 09:24:16 -0700

    removed -c defaults -c cdat/label/unstable from conda_upload.sh

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -35,7 +35,8 @@ ln -s ../recipe cdms2
 export BRANCH=${CIRCLE_BRANCH}
 python ./prep_for_build.py  -b ${BRANCH}
 echo "conda build ${PKG_NAME} -c defaults -c cdat/label/unstable -c conda-forge"
-conda build ${PKG_NAME} -c defaults -c cdat/label/unstable -c conda-forge
+# conda build ${PKG_NAME} -c defaults -c cdat/label/unstable -c conda-forge
+conda build ${PKG_NAME} -c conda-forge
 
 echo "anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $LABEL $CONDA_BLD_PATH/$OS/${PKG_NAME}-$VERSION.`date +%Y*`0.tar.bz2 --force"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $LABEL $CONDA_BLD_PATH/$OS/${PKG_NAME}-$VERSION.`date +%Y*`0.tar.bz2 --force

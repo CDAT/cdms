afd2aa7904b23a7cc6bfdccaafee4b4f847ccca4
muryanto1 <muryanto1@llnl.gov>
2020-01-07 09:28:44 -0800

    set CONDA_FORGE_CFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export CONDA_FORGE_CFLAGS="x86_64-apple-darwin13.4.0-clang -fno-strict-aliasing -Wsign-compare -Wunreachable-code -DNDEBUG -fwrapv -O3 -Wall -Wstrict-prototypes -march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -O3 -pipe -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=$PREFIX=/usr/local/src/conda-prefix -flto -Wl,-export_dynamic -march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O3 -w -g -O0 -I$PREFIX/include -D_FORTIFY_SOURCE=2 -mmacosx-version-min=10.9 -isystem $PREFIX/include -Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
 export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} ${CFLAGS} ${CXXFLAGS}"
 export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} -stdlib=libc++"
 export CONDA_FORGE_LDFLAGS="${CONDA_FORGE_LDFLAGS} -L${PREFIX}/lib ${LDFLAGS}"
@@ -12,6 +11,8 @@ then
     # for Mac OSX                                                                                                                          
     export CC=clang
     export CXX=clang++
+    export CONDA_FORGE_CFLAGS="x86_64-apple-darwin13.4.0-clang -fno-strict-aliasing -Wsign-compare -Wunreachable-code -DNDEBUG -fwrapv -O3 -Wall -Wstrict-prototypes -march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -O3 -pipe -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=$PREFIX=/usr/local/src/conda-prefix -flto -Wl,-export_dynamic -march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O3 -w -g -O0 -I$PREFIX/include -D_FORTIFY_SOURCE=2 -mmacosx-version-min=10.9 -isystem $PREFIX/include -Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
+
     if [ ${HOME} == "/Users/distiller" ]; then
         export CONDA_FORGE_CFLAGS="${CONDA_FORGE_CFLAGS} -Wl,-syslibroot / -isysroot /"
     fi  

d9a51ee8a86f3f4859c278b387e3a545fe3cc772
muryanto1 <muryanto1@llnl.gov>
2020-01-09 09:28:59 -0800

    removed export CFLAGS, etc in recipe/build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,29 +1,6 @@
-#!/bin/bash
-export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
-export CXXLAGS="${CFLAGS} ${CXXFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-export LFLAGS="-fPIC ${LFLAGS}"
-export FC=""
+#!/bin/bash -x
 
-set -x
-
-#if [ `uname` == Linux ]; then
-#    # To make sure we get the correct g++
-#    export LD_LIBRARY_PATH=${PREFIX}/lib:${LIBRARY_PATH}
-#    export CC="gcc -Wl,-rpath=${PREFIX}/lib"
-#    export CXX="g++ -Wl,-rpath=${PREFIX}/lib"
-#else
-#    export CC="gcc"
-#    export CXX="g++"
-#fi
-#python setup.py install
 if [ $(uname) == "Linux" ];then
     export LDSHARED="$CC -shared -pthread"
-    LDSHARED="$CC -shared -pthread" python setup.py install
-else
-    if [ ${HOME} == "/Users/distiller" ]; then
-        export  CFLAGS="-Wl,-syslibroot / -isysroot / ${CFLAGS}"
-    fi
-    python setup.py install
 fi
+python setup.py install

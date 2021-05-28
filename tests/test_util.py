292f3c8f3b402821adac6eb8ccc3943cdf7fbae5
Jason Boutte <boutte.jason@gmail.com>
2021-05-28 13:33:05 -0700

    Updates util unittest

--- tests/test_util.py
+++ tests/test_util.py
@@ -12,10 +12,20 @@ class TestUtil(unittest.TestCase):
         with self.assertRaises(CDMSError):
             util.getenv_bool("TEST")
 
-        with patch.dict(os.environ, {"TEST": "true"}):
-            value = util.getenv_bool("TEST")
+        tests = {
+            "true": True,
+            "True": True,
+            "false": False,
+            "False": False
+        }
 
-        assert value == True
+        for x, y in tests.items():
+            with patch.dict(os.environ, {"TEST": x}):
+                value = util.getenv_bool("TEST")
+
+                assert value == y
+
+        assert util.getenv_bool("NOEXIST", "false") == False
 
     def test_getenv_bool_invalid(self):
         with contextlib.ExitStack() as stack:
